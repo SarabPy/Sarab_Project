e2d36ce33196ce5aa8516606bcb984ae0846fe36
Anas-Mersal <anas.ali@uob.edu.ly>
2024-02-21 14:15:09 +0200

    first commit

---
+++ Test_Folder/frontend.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import requests
+
+# Streamlit form for user input
+st.title('Data Submission Form')
+
+with st.form("user_data_form"):
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    phone = st.text_input("Phone")
+    transfer_limit = st.number_input("Transfer Limit", min_value=0.0, format='%f')
+    nid = st.text_input("National ID")
+    
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        data = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "transfer_limit": transfer_limit,
+            "nid": nid
+        }
+        
+        response = requests.post("http://localhost:8000/process-data/", json=data) #192.168.1.130:8000/process-data/ or localhost:8000/process-data/
+        if response.status_code == 200:
+            result_data = response.json()
+            st.success("Data Processed Successfully!")
+            st.write(result_data)
+        else:
+            st.error("An error occurred.")
---
+++ frontend.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+
+# Streamlit form for user input
+st.title('Data Submission Form')
+
+with st.form("user_data_form"):
+    name = st.text_input("Name")
+    email = st.text_input("Email")
+    phone = st.text_input("Phone")
+    transfer_limit = st.number_input("Transfer Limit", min_value=0.0, format='%f')
+    nid = st.text_input("National ID")
+    
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        data = {
+            "name": name,
+            "email": email,
+            "phone": phone,
+            "transfer_limit": transfer_limit,
+            "nid": nid
+        }
+        
+        response = requests.post("https://sarab.onrender.com/process-data/", json=data) #192.168.1.130 or localhost:8000
+        if response.status_code == 200:
+            result_data = response.json()
+            st.success("Data Processed Successfully!")
+            st.write(result_data)
+        else:
+            st.error("An error occurred.")
+
